9c8536c3646ad1215622874725783e80c0d87187
MagicKrumpli <S00274238@atu.ie>
2026-09-23 09:58:15 +0100

    Revert "worksheet2 done"
    This reverts commit 217f5e5da887d14f9b9e04c1dc6ff1014b72ecba.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,8 @@
 import express, {Application, Request, Response} from "express" ;
-// import userRoutes from './routes/';
-import carRoutes from './routes/cars';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
-
-app.use(express.json());
-app.use('/api/v1/cars', carRoutes);
 
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
